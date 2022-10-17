9dafe68ade15acba94847b014b4e0addb8e2a4d4
uluibraphp <ibrahimulu_77@hotmail.com>
2022-10-17 11:32:16 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp1011s1s200456630 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.comp1011s1s200456630 to javafx.fxml;
+    exports com.example.comp1011s1s200456630;
+}
